9340c12a07834a5f872e3645561cb8f75d6d4d33
David Paulsson <david@davidpaulsson.se>
2016-10-20 11:29:09 +0200

    Footer tel links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import restaurants from './../data/restaurants.json';
 import logo from './../assets/logo.svg';
 import './Footer.css';
 
@@ -8,6 +9,10 @@ class Footer extends React.Component {
     constructor(props) {
         super(props);
         this.displayName = 'Footer';
+    }
+
+    getNumberLink(number) {
+        return 'tel:0046' + number.slice(1).replace(/ /g,'').replace(/-/g,'');
     }
 
     render() {
@@ -89,12 +94,20 @@ class Footer extends React.Component {
                     </div>
                     <div className="footer__col">
                         <ul className="footer-menu__list">
-                            <li className="footer-menu__list-item">
-                                <svg style={{ width: '18px', height: '18px' }} viewBox="0 0 24 24">
-                                    <path fill="#333333" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" />
-                                </svg>
-                                073-123 45 67
-                            </li>
+                            { restaurants.map(restaurant => (
+                                <li key={ restaurant.slug } className="footer-menu__list-item">
+                                    <svg style={{ width: '18px', height: '18px' }} viewBox="0 0 24 24">
+                                        <path fill="#333333" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" />
+                                    </svg>
+                                    { restaurant.name }:
+                                    <a
+                                        style={{ marginLeft: '.25em' }}
+                                        href={ this.getNumberLink(restaurant.phoneNumber) }
+                                    >
+                                        { restaurant.phoneNumber }
+                                    </a>
+                                </li>
+                            )) }
                             <li className="footer-menu__list-item">
                                 <svg style={{ width: '18px', height: '18px' }} viewBox="0 0 24 24">
                                     <path fill="#333333" d="M20,4H4A2,2 0 0,0 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6A2,2 0 0,0 20,4M20,18H4V8L12,13L20,8V18M20,6L12,11L4,6V6H20V6Z" />
