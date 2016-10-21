9c60d8ea451d3e6aaf0ce54e154dccc5bf074c67
David Paulsson <david@davidpaulsson.se>
2016-10-21 12:09:49 +0200

    No match

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './NoMatch.css';
+
 class NoMatch extends React.Component {
     constructor(props) {
         super(props);
@@ -7,12 +9,19 @@ class NoMatch extends React.Component {
     }
 
     render() {
+        const { location } = this.props;
+
         return (
-            <div>NoMatch</div>
+            <div className="no-match">
+                <h1 className="no-match__title">Not Found</h1>
+                <p style={{ textAlign: 'center' }}>The requested URL <code>{ location.pathname }</code> was not found on this server.</p>
+            </div>
         );
     }
 }
 
-NoMatch.propTypes = {};
+NoMatch.propTypes = {
+    location: React.PropTypes.object
+};
 
 export default NoMatch;
