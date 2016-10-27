57682d37c4118e505ba8b82422bcf5aa14626165
David Paulsson <david@davidpaulsson.se>
2016-10-27 15:28:46 +0200

    Translate footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 
 import restaurants from './../data/restaurants.json';
 import logo from './../assets/logo.svg';
+
 import './Footer.css';
 
 class Footer extends React.Component {
@@ -12,10 +13,15 @@ class Footer extends React.Component {
     }
 
     getNumberLink(number) {
-        return 'tel:0046' + number.slice(1).replace(/ /g, '').replace(/-/g, '');
+        return 'tel:0046' + number
+                                .slice(1)
+                                .replace(/ /g, '')
+                                .replace(/-/g, '');
     }
 
     render() {
+        const { locale } = this.props;
+
         return (
             <div className="footer-wrapper">
                 <div className="footer">
@@ -29,7 +35,7 @@ class Footer extends React.Component {
                                     <Link
                                         className="footer-menu__link"
                                         to="/">
-                                        Home
+                                        { locale === 'en' ? 'Home' : 'Hem' }
                                     </Link>
                                 </li>
                                 {/*<li className="footer-menu__list-item">
@@ -52,14 +58,14 @@ class Footer extends React.Component {
                                     <Link
                                         className="footer-menu__link"
                                         to="/contact-us">
-                                        Contact Us
+                                        { locale === 'en' ? 'Contact Us' : 'Kontakta oss' }
                                     </Link>
                                 </li>
                                 <li className="footer-menu__list-item">
                                     <Link
                                         className="footer-menu__link"
                                         to="/careers">
-                                        Work With SEN
+                                        { locale === 'en' ? 'Work With SEN' : 'Arbeta med SEN' }
                                     </Link>
                                 </li>
                             </ul>
@@ -78,7 +84,8 @@ class Footer extends React.Component {
                                     <svg style={{ width: '18px', height: '18px' }} viewBox="0 0 24 24">
                                         <path fill="#333333" d="M19,4V7H17A1,1 0 0,0 16,8V10H19V13H16V20H13V13H11V10H13V7.5C13,5.56 14.57,4 16.5,4M20,2H4A2,2 0 0,0 2,4V20A2,2 0 0,0 4,22H20A2,2 0 0,0 22,20V4C22,2.89 21.1,2 20,2Z" />
                                     </svg>
-                                    <a href="https://www.facebook.com/pages/Sen-Street-Kitchen-Ume%C3%A5/1525861991050193">Umeå</a>                                </li>
+                                    <a href="https://www.facebook.com/pages/Sen-Street-Kitchen-Ume%C3%A5/1525861991050193">Umeå</a>
+                                </li>
                                 <li className="footer-menu__list-item">
                                     <svg style={{ width: '18px', height: '18px' }} viewBox="0 0 24 24">
                                         <path fill="#333333" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
@@ -127,6 +134,8 @@ class Footer extends React.Component {
     }
 }
 
-Footer.propTypes = {};
+Footer.propTypes = {
+    locale: React.PropTypes.string
+};
 
 export default Footer;
