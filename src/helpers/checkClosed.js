290ea0840025e093aa428e66a25883f6b33765fe
David Paulsson <david@davidpaulsson.se>
2018-01-08 09:20:13 +0100

    Update opening hours

--- src/helpers/checkClosed.js
+++ src/helpers/checkClosed.js
@@ -6,45 +6,15 @@ const checkClosed = ({string, restaurant, locale, deviation = false}) => {
         switch (restaurant) {
         case 'regeringsgatan-26':
             switch (today) {
-            case '11-4':
-                string = '10-17';
+            case '12-27':
+            case '12-28':
+            case '12-29':
+                string = '11-15';
                 break;
-            case '7-17':
-            case '7-18':
-            case '7-19':
-            case '7-20':
-            case '7-21':
-            case '7-23':
-            case '7-24':
-            case '7-25':
-            case '7-26':
-            case '7-27':
-            case '7-28':
-            case '7-30':
-            case '7-31':
-            case '8-1':
-            case '8-2':
-            case '8-3':
-            case '8-5':
-            case '8-6':
-                string = '10-15';
-                break;
+            case '12-30':
             case '1-1':
             case '1-6':
-            case '6-24':
-            case '7-1':
-            case '7-15':
-            case '7-22':
-            case '7-29':
-            case '7-8':
-            case '8-12':
-            case '8-4':
-            case '11-5':
-            case '12-24':
-            case '12-25':
-            case '12-26':
-            case '12-31':
-            case '12-5':
+            case '1-7':
                 string = 'Closed';
                 break;
             default:
@@ -53,19 +23,7 @@ const checkClosed = ({string, restaurant, locale, deviation = false}) => {
             break;
         case 'kungsbron-8':
             switch (today) {
-            case '1-1':
-            case '1-2':
-            case '1-3':
-            case '1-4':
-            case '1-5':
-            case '1-6':
-            case '7-24':
-            case '7-25':
-            case '7-26':
-            case '7-27':
-            case '7-28':
-            case '7-29':
-            case '7-30':
+            case '12-23':
             case '12-24':
             case '12-25':
             case '12-26':
@@ -74,7 +32,14 @@ const checkClosed = ({string, restaurant, locale, deviation = false}) => {
             case '12-29':
             case '12-30':
             case '12-31':
+            case '1-1':
                 string = 'Closed';
+                break;
+            case '1-2':
+            case '1-3':
+            case '1-4':
+            case '1-5':
+                string = '11-15';
                 break;
             default:
                 break;
@@ -82,36 +47,14 @@ const checkClosed = ({string, restaurant, locale, deviation = false}) => {
             break;
         case 'avion-shopping':
             switch (today) {
-            case '11-5':
-                string = '10-18';
-                break;
-            case '12-3':
-            case '12-10':
-                string = '09-19';
-                break;
-            case '12-17':
-            case '12-18':
-            case '12-19':
-            case '12-20':
-            case '12-21':
-            case '12-22':
-            case '12-23':
-                string = '09-21';
-                break;
-            case '12-24':
-            case '12-31':
             case '1-1':
                 string = '10-15';
                 break;
-            case '12-25':
-                string = 'Closed';
-                break;
-            case '12-26':
-            case '1-6':
-                string = '10-18';
-                break;
             case '1-5':
                 string = '10-20';
+                break;
+            case '1-6':
+                string = '10-18';
                 break;
             default:
                 break;
